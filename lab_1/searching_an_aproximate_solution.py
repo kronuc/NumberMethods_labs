3cd5cd59c8765dea8f9b781d17ef11ecba242d70
kronuc <denerator2@gmail.com>
2021-02-15 12:40:39 +0200

    write script for drawing graph

---
+++ lab_1/searching_an_aproximate_solution.py
@@ -0,0 +1,16 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x1 = np.arange(-1.5, -0.5, 0.005)
+y1 = np.power(x1,2) - 3
+
+x2 = np.arange(-1.5, -0.5, 0.005)
+y2 = np.power(x2,5)
+
+fig, ax = plt.subplots()
+ax.plot(x1,y1)
+ax.plot(x2,y2)
+ax.set(xlabel='x', ylabel='y')
+ax.grid()
+plt.show()
